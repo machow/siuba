6e9c474fc2527cf9eec64ee4ee39f72b0de8581c
Michael Chow <machow@princeton.edu>
2019-02-27 23:02:35 -0500

    add postgres specific dialect file

---
+++ siuba/sql/dialects/postgres.py
@@ -0,0 +1,18 @@
+# sqlvariant, allow defining 3 namespaces to override defaults
+from ..translate import base_scalar, base_agg, base_win, SqlTranslator
+import sqlalchemy.sql.sqltypes as sa_types
+from sqlalchemy import sql
+
+def sql_round(col, n):
+    return sql.func.round(sql.cast(col, sa_types.Numeric()), n)
+
+scalar = SqlTranslator(
+        base_scalar,
+        round = sql_round
+        )
+
+aggregate = base_agg
+
+window = base_win
+
+funcs = dict(scalar = scalar, aggregate = aggregate, window = window)
