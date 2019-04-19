ead8f1b38869bf730de75eaeff51ac304eb78fbe
Michael Chow <machow@princeton.edu>
2019-04-19 15:24:25 -0400

    add stub for sqlite dialect

--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@ NOTEBOOK_TESTS=$(addprefix examples/, examples-dplyr-funcs.ipynb case-iris-selec
 
 test:
 	py.test --nbval $(NOTEBOOK_TESTS)
+	py.test
 
 test-travis:
 	py.test --nbval $(filter-out %postgres.ipynb, $(NOTEBOOK_TESTS))
+	py.test
 
 examples/%.ipynb:
 	jupyter nbconvert --to notebook --inplace --execute $@
---
+++ siuba/sql/dialects/sqlite.py
@@ -0,0 +1,21 @@
+# sqlvariant, allow defining 3 namespaces to override defaults
+from ..translate import base_scalar, base_agg, base_win, SqlTranslator, win_agg
+import sqlalchemy.sql.sqltypes as sa_types
+from sqlalchemy import sql
+
+scalar = SqlTranslator(
+        base_scalar,
+        )
+
+aggregate = SqlTranslator(
+        base_agg
+        )
+
+window = SqlTranslator(
+        # TODO: should check sqlite version, since < 3.25 can't use windows
+        base_win,
+        sd = win_agg("stddev")
+        )
+
+funcs = dict(scalar = scalar, aggregate = aggregate, window = window)
+
