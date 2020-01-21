421786b24ccf5afa0b167e96ad788a38ce8c55fa
Michael Chow <machow@princeton.edu>
2020-01-21 10:55:20 -0500

    refactor: clean up grouped pandas translations

--- siuba/experimental/pd_groups/translate.py
+++ siuba/experimental/pd_groups/translate.py
@@ -2,82 +2,94 @@ from .groupby import DataFrameGroupBy, GroupByAgg, SeriesGroupBy, broadcast_grou
 import pandas as pd
 
 
-def is_literal(el):
-    # TODO: pandas has this function--should use that
-    return isinstance(el, (int, float, str))
+# utilities -------------------------------------------------------------------
+
+def _validate_data_args(x, **kwargs):
+    if not isinstance(x, SeriesGroupBy):
+        raise TypeError("First data argument must be a grouped Series object")
+
+    for name, other_data in kwargs.items():
+        if isinstance(other_data, pd.Series):
+            raise TypeError("{} may not be a Series.".format(name))
+
+
+def _apply_grouped_method(ser, name, is_property, accessor, args, kwargs):
+    if accessor:
+        method = getattr(getattr(ser, accessor), name)
+    else:
+        method = getattr(ser, name)
+
+    res = method(*args, **kwargs) if not is_property else method
+
+    return res
+
+
+def _maybe_broadcast(x, y):
+    """Same as broadcast_group_elements, but y doesn't have to be SeriesGroupBy
+
+    This is important when y is a literal (e.g. 1), since we don't want to raise
+    an error, or broadcast 1 to the length of x. Rather, we want to keep the literal,
+    and let the pandas series handle it in the operation.
+
+    """
+    if isinstance(y, SeriesGroupBy):
+        left, right, groupby = broadcast_group_elements(x, y)
+    else:
+        left, right, groupby = x.obj, y, x
+
+    return left, right, groupby
+
+
+# Translations ----------------------------------------------------------------
 
 def not_implemented(name, is_property, accessor):
     return NotImplementedError
 
+
 def method_agg_op(name, is_property, accessor):
     def f(__ser, *args, **kwargs):
-        if not isinstance(__ser, SeriesGroupBy):
-            raise TypeError("All methods must operate on grouped Series objects")
-        
-        method = getattr(__ser, name)
+        _validate_data_args(__ser)
+        res = _apply_grouped_method(__ser, name, is_property, accessor, args, kwargs)
 
-        res = method(*args, **kwargs)
         return GroupByAgg.from_result(res, __ser)
 
-    f.__name__ = name
-    f.__qualname__ = name
+    f.__name__ = f.__qualname__ = name
     return f
+
 
 def method_el_op(name, is_property, accessor):
     def f(__ser, *args, **kwargs):
-        if not isinstance(__ser, SeriesGroupBy):
-            raise TypeError("All methods must operate on a grouped Series objects")
-        
-        if accessor:
-            method = getattr(getattr(__ser.obj, accessor), name)
-        else:
-            method = getattr(__ser.obj, name)
+        _validate_data_args(__ser)
+        res = _apply_grouped_method(__ser.obj, name, is_property, accessor, args, kwargs)
 
-        res = method(*args, **kwargs) if not is_property else method
         return _regroup(res, __ser)
 
-    f.__name__ = name
-    f.__qualname__ = name
+    f.__name__ = f.__qualname__ = name
     return f
 
-def method_el_op2(name, **kwargs):
-    def f(x, y):
-        if isinstance(x, pd.Series) or isinstance(y, pd.Series):
-            raise TypeError("No Series allowed")
 
-        elif isinstance(x, SeriesGroupBy) and isinstance(y, SeriesGroupBy):
-            left, right, groupby = broadcast_group_elements(x, y)
-        elif is_literal(x):
-            right, left, groupby = x, y.obj, y
-        elif is_literal(y):
-            left, right, groupby = x.obj, y, x
-        else:
-            raise TypeError("All methods must operate on a grouped Series objects")
+def method_el_op2(name, is_property, accessor):
+    def f(x, y):
+        _validate_data_args(x, y = y)
+        left, right, groupby = _maybe_broadcast(x, y)
         
         op_function = getattr(left, name)
 
         res = op_function(right)
         return _regroup(res, groupby)
 
-    f.__name__ = name
-    f.__qualname__ = name
+    f.__name__ = f.__qualname__ = name
     return f
+
 
 def method_win_op(name, is_property, accessor):
     def f(__ser, *args, **kwargs):
-        if not isinstance(__ser, SeriesGroupBy):
-            raise TypeError("All methods must operate on a grouped Series objects")
-        
-        if accessor:
-            method = getattr(getattr(__ser, accessor), name)
-        else:
-            method = getattr(__ser, name)
+        _validate_data_args(__ser)
+        res = _apply_grouped_method(__ser, name, is_property, accessor, args, kwargs)
 
-        res = method(*args, **kwargs) if not is_property else method
         return _regroup(res, __ser)
 
-    f.__name__ = name
-    f.__qualname__ = name
+    f.__name__ = f.__qualname__ = name
     return f
 
 
