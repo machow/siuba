a8098a8fdf72c2fd14f7bc9dec3506273b5e6ffe
Michael Chow <mc_al_github@fastmail.com>
2020-05-05 11:30:00 -0400

    feat: new cars, cars_sql data sources

--- siuba/data/__init__.py
+++ siuba/data/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pkg_resources
 
+# mtcars ----------------------------------------------------------------------
 _fname = pkg_resources.resource_filename("siuba.data", "mtcars.csv")
 
 mtcars = pd.read_csv(_fname)
@@ -11,3 +12,18 @@ Source: Henderson and Velleman (1981), Building multiple regression models inter
 
 --- Original DataFrame docs below ---
 """ + mtcars.__doc__
+
+
+# cars ------------------------------------------------------------------------
+cars = mtcars[["cyl", "mpg", "hp"]]
+
+
+# cars_sql --------------------------------------------------------------------
+import siuba.sql.utils as _sql_utils
+from siuba.sql import LazyTbl as _LazyTbl
+cars_sql = _LazyTbl(
+        _sql_utils.mock_sqlalchemy_engine("postgresql"),
+        "cars",
+        ["cyl", "mpg", "hp"]
+        )
+
