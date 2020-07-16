8c6de7a7b13b2349e5623104acf759c6a81da50e
Timothy Mastny <tim.mastny@gmail.com>
2020-07-15 21:33:31 -0500

    add context manager to add and reset attributes in shell

--- siuba/completer.py
+++ siuba/completer.py
@@ -1,18 +1,33 @@
 from IPython import get_ipython
 
 
+class ShellCompletion(object):
+    def __init__(self, shell, target_df):
+        self.old_ns = shell.user_ns
+
+        old_ns = shell.Completer.namespace
+        target_df = shell.user_ns["mtcars"]
+        shell.Completer.namespace = {**old_ns, "_": target_df}
+
+        self.old_custom_completers = shell.Completer.custom_completers
+        shell.Completer.custom_completers = None
+
+        self.shell = shell
+
+    def __enter__(self):
+        return self.shell
+
+    def __exit__(self):
+        shell = self.shell
+
+        shell.Completer.namespace = self.old_ns
+        shell.Completer.custom_completers = self.old_custom_completers
+
+
 def symbolic_completer(shell, event):
-    old_ns = shell.Completer.namespace
-    target_df = shell.user_ns["mtcars"]
-    shell.Completer.namespace = {**old_ns, "_": target_df}
 
-    old_custom_completers = shell.Completer.custom_completers
-    shell.Completer.custom_completers = None
-
-    _, _, _, jedi = shell.Completer.complete(event.symbol)
-
-    shell.Completer.namespace = old_ns
-    shell.Completer.custom_completers = old_custom_completers
+    with ShellCompletion(shell, "mtcars") as shell:
+        _, _, _, jedi = shell.Completer.complete(event.symbol)
 
     return [
         event.symbol + completions.name_with_symbols for completions in jedi
