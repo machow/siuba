729d72a8e93ec627c81c0c584a8389b00d3d0d25
Michael Chow <mc_al_github@fastmail.com>
2020-11-12 17:38:24 -0500

    DRAFT: add data for pivot vignette

--- siuba/data/__init__.py
+++ siuba/data/__init__.py
@@ -1,10 +1,37 @@
-import pandas as pd
-import pkg_resources
+import pandas as _pd
+import sys
+from pkg_resources import resource_filename as _rf
 
-# mtcars ----------------------------------------------------------------------
-_fname = pkg_resources.resource_filename("siuba.data", "mtcars.csv")
+__all__ = [
+    "mtcars", "cars", "cars_sql",
+    "relig_income"
+    ]
 
-mtcars = pd.read_csv(_fname)
+# Utils =======================================================================
+
+def _load_df(name):
+    # There is an artist in the data named "N/A", so tell pandas it's not an NA
+    return _pd.read_csv(_rf("siuba.data", name + ".csv"))
+
+def __getattr__(x):
+    if x in __all__:
+        df = _load_df(x)
+        globals()[x] = df
+        return x
+
+    raise AttributeError("Only these datasets are defined: %s" % __all__)
+
+if sys.version_info[0] == 3 and sys.version_info[1] < 7:
+    # __getattr__ implemented in python 3.7, so for earlier versions need to
+    # load all the data up front.
+    for _fname in set(__all__) - {"cars", "cars_sql"}:
+        globals()[_fname] = _load_df(_fname)
+
+# mtcars ======================================================================
+
+_fname = _rf("siuba.data", "mtcars.csv")
+
+mtcars = _pd.read_csv(_fname)
 mtcars.__doc__ = """
 mtcars data.
 
@@ -26,4 +53,7 @@ cars_sql = _LazyTbl(
         "cars",
         ["cyl", "mpg", "hp"]
         )
+
+
+# Tidyr data ==================================================================
 
