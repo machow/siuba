c1317a2a788f9ec5080c8ffdfc8d34cc9236a339
Michael Chow <mc_al_github@fastmail.com>
2021-04-30 09:31:33 -0400

    fix: allow piping symbolic attributes like _.a

--- siuba/tests/test_verb_utils.py
+++ siuba/tests/test_verb_utils.py
@@ -1,9 +1,12 @@
+from siuba.siu import Symbolic
 from siuba.sql.verbs import collect, show_query, LazyTbl
 from siuba.dply.verbs import Pipeable
 from .helpers import data_frame
 import pandas as pd
 
 import pytest
+
+_ = Symbolic()
 
 @pytest.fixture(scope = "module")
 def df(backend):
@@ -18,3 +21,9 @@ def test_collect(df):
     assert isinstance(collect(df), pd.DataFrame)
     assert isinstance(df >> collect(), pd.DataFrame)
     assert isinstance(collect(), Pipeable)
+
+def test_pipe_symbolic_attr():
+    class A: a = 1
+
+    assert Pipeable(_.a)(A) == 1
+
