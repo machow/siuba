3f7f868acdef3485c2ba06ae361c9fb8d2d49fb6
Michael Chow <mc_al_github@fastmail.com>
2022-01-20 20:39:34 -0500

    fix(siu): re-expose functions referenced in docs

--- siuba/siu/__init__.py
+++ siuba/siu/__init__.py
@@ -1,6 +1,15 @@
-from .calls import Call, MetaArg, FuncArg, Lazy, BinaryOp, _SliceOpIndex, DictCall, str_to_getitem_call
+from .calls import (
+    Call,
+    MetaArg,
+    FuncArg,
+    Lazy,
+    BinaryOp,
+    _SliceOpIndex,
+    DictCall,
+    str_to_getitem_call
+)
 from .symbolic import Symbolic, strip_symbolic, create_sym_call, explain
-from .visitors import CallTreeLocal, FunctionLookupBound, FunctionLookupError
+from .visitors import CallTreeLocal, CallVisitor, FunctionLookupBound, FunctionLookupError
 from .dispatchers import symbolic_dispatch, singledispatch2, pipe_no_args, Pipeable
 
 Lam = Lazy
