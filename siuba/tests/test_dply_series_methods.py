7d7d60e90bbea8f4c4c5821301990b93552a0321
Michael Chow <mc_al_github@fastmail.com>
2022-03-27 19:01:21 -0400

    tests(sql): re-enable more sqlite tests

--- siuba/tests/test_dply_series_methods.py
+++ siuba/tests/test_dply_series_methods.py
@@ -231,7 +231,6 @@ def test_pandas_grouped_frame_fast_not_implemented(notimpl_entry):
 
 
 #@backend_pandas
-#@pytest.mark.skip_backend('sqlite')
 def test_frame_mutate(skip_backend, backend, entry):
     do_test_missing_implementation(entry, backend)
     skip_no_mutate(entry, backend)
@@ -296,7 +295,6 @@ def test_pandas_grouped_frame_fast_mutate(entry):
     assert_frame_equal(res_obj, dst.obj)
 
 
-@pytest.mark.skip_backend('sqlite')
 def test_frame_summarize(skip_backend, backend, agg_entry):
     entry = agg_entry
 
