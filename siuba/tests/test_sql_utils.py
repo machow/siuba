bc6fdb66802451c7de6f6f7fa9e6be0140b549ec
Michael Chow <mc_al_github@fastmail.com>
2022-03-29 17:57:25 -0400

    fix: mock_sqlalchemy_engine across dialects, collecting

--- siuba/tests/test_sql_utils.py
+++ siuba/tests/test_sql_utils.py
@@ -1,4 +1,6 @@
-from siuba.sql.utils import get_dialect_translator
+from siuba.sql.utils import get_dialect_translator, mock_sqlalchemy_engine
+from siuba.sql.verbs import collect
+from siuba.sql import LazyTbl
 import pytest
 
 @pytest.mark.parametrize('name', [
@@ -8,3 +10,15 @@ import pytest
     ])
 def test_get_dialect_translator(name):
     get_dialect_translator(name)
+
+def test_mock_sqlalchemy_engine_dialect():
+    engine = mock_sqlalchemy_engine("postgresql")
+    assert engine.dialect.name == "postgresql"
+
+    engine = mock_sqlalchemy_engine("sqlite")
+    assert engine.dialect.name == "sqlite"
+
+def test_mock_sqlalchemy_engine_no_collect():
+    engine = mock_sqlalchemy_engine("sqlite")
+    tbl = LazyTbl(engine, "some_table", ["x"])
+    assert collect(tbl) is None
