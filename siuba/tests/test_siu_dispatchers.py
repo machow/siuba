87c0e5c49f359abfd3ffd66d9b173c137e168736
Michael Chow <mc_al_github@fastmail.com>
2022-07-18 10:41:37 -0400

    tests: change some pipe tests to use call

--- siuba/tests/test_siu_dispatchers.py
+++ siuba/tests/test_siu_dispatchers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from siuba.siu.dispatchers import pipe
+from siuba.siu.dispatchers import call
 from siuba.siu import _
 
 # calls a function on data
@@ -10,32 +10,32 @@ from siuba.siu import _
 # siu expr first arg not extra args allowed
 # siu expr first arg simple
 # "abc", 
-def test_siu_pipe_no_args():
-    assert 1 >> pipe(range) == range(1)
+def test_siu_call_no_args():
+    assert 1 >> call(range) == range(1)
 
-def test_siu_pipe_no_args_explicit():
-    assert 1 >> pipe(range, _) == range(1)
+def test_siu_call_no_args_explicit():
+    assert 1 >> call(range, _) == range(1)
 
-def test_siu_pipe_pos_arg():
-    assert 1 >> pipe(range, _, 2) == range(1, 2)
-
-
-def test_siu_pipe_kwarg():
-    assert "," >> pipe("a,b,c".split, _, maxsplit=1) == ["a", "b,c"]
+def test_siu_call_pos_arg():
+    assert 1 >> call(range, _, 2) == range(1, 2)
 
 
-def test_siu_pipe_arg_kwarg():
-    assert 1 >> pipe("{0}_{1}_{b}".format, _, 2, b=3) == "1_2_3"
+def test_siu_call_kwarg():
+    assert "," >> call("a,b,c".split, _, maxsplit=1) == ["a", "b,c"]
 
 
-def test_siu_pipe_underscore_arg():
-    assert 1 >> pipe(range, 2, 3, _) == range(2, 3, 1)
+def test_siu_call_arg_kwarg():
+    assert 1 >> call("{0}_{1}_{b}".format, _, 2, b=3) == "1_2_3"
 
 
-def test_siu_pipe_underscore_method():
-    assert "a,b" >> pipe(_.split(",")) == ["a", "b"]
+def test_siu_call_underscore_arg():
+    assert 1 >> call(range, 2, 3, _) == range(2, 3, 1)
 
 
-def test_siu_pipe_understcore_method_args():
+def test_siu_call_underscore_method():
+    assert "a,b" >> call(_.split(",")) == ["a", "b"]
+
+
+def test_siu_call_understcore_method_args():
     with pytest.raises(NotImplementedError):
-        "a,b" >> pipe(_.split, _, ",")
+        "a,b" >> call(_.split, _, ",")
